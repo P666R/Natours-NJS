5e627eb5c320e00c44040d36dd71dff0483948ef
PRoy <sam469roger@gmail.com>
2023-04-29 19:08:48 +0530

    custom middleware

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ const app = express();
 // middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('hello from middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 // route handlers
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
