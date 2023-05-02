4d3a8cd5c629e8c718725e27d7c970a9d7f088e1
PRoy <sam469roger@gmail.com>
2023-05-02 16:21:46 +0530

    setup env

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // middleware
-
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('hello from middleware');
