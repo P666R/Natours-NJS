2135494967fccbf77bb1539c47d1516b7a63b151
PRoy <sam469roger@gmail.com>
2023-05-05 07:36:31 +0530

    connected DB with app

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
+/*
+const DB = process.env.DATABASE_LOCAL;
+*/
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`app running on port ${port}...`);
-});
+
+const start = async () => {
+  try {
+    await mongoose
+      .connect(DB)
+      .then(() => console.log('DB connection successful'));
+
+    app.listen(port, () => console.log(`app running on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
