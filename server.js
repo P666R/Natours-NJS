14a3af0d67eaf47e5e044099b68c50587b31ebb1
PRoy <sam469roger@gmail.com>
2023-05-06 20:02:46 +0530

    created model & documents

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-/*
-const DB = process.env.********LOCAL;
-*/
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE_LOCAL;
 
 const port = process.env.PORT || 3000;
 
@@ -26,5 +24,33 @@ const start = async () => {
     console.log(error);
   }
 };
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log('testTour error', err));
 
 start();
