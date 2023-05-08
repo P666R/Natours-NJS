d33516f6294d3aabb244be103c4901829f395a9a
PRoy <sam469roger@gmail.com>
2023-05-08 21:45:43 +0530

    importing data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// const DB = process.env.DATABASE_LOCAL;
+
+mongoose.connect(DB).then(() => console.log('DB connection successful'));
+
+// read json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// import data into database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data successfully loaded');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// delete all data from collection
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data successfully deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
