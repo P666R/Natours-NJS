a696cd2dfaecfdc8f3764b1c31e811a867aa6ea9
PRoy <sam469roger@gmail.com>
2023-05-08 19:58:37 +0530

    crud ops implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -74,4 +74,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {};
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
