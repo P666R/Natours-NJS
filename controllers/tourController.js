81a3f29a96c1821ce3d97979663495e3953808a9
PRoy <sam469roger@gmail.com>
2023-05-10 23:50:45 +0530

    aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,13 @@
 const Tour = require('../models/tourModel');
 
+// midddleware
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,price,summary,difficulty';
+  next();
+};
+
 // route handlers
 exports.getAllTours = async (req, res) => {
   try {
