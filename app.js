95f16bf5ee30980838b21c45f8a221317dd9b438
PRoy <sam469roger@gmail.com>
2023-05-17 04:35:45 +0530

    handling unhandled routes

--- app.js
+++ app.js
@@ -17,18 +17,19 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('hello from middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 // routes
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
