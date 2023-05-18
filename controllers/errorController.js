af983767a817f9f185cad234877db0d8b1c17e97
PRoy <sam469roger@gmail.com>
2023-05-19 03:39:34 +0530

    handling mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,6 +10,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -52,6 +58,9 @@ module.exports = (err, req, res, next) => {
 
     if (err.code === 11000)
       return sendErrorProd(handleDuplicateFieldsDB(err), res);
+
+    if (err.name === 'ValidationError')
+      return sendErrorProd(handleValidationErrorDB(err), res);
 
     sendErrorProd(err, res);
   }
