a7f85f202fe34bf78336485f0f98ada1a9c038f9
PRoy <sam469roger@gmail.com>
2023-05-26 15:28:10 +0530

    signing up users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,51 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // sync version
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
+
+/* async implementation
+  jwt.sign(
+    { id: newUser._id },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    },
+    (err, token) => {
+      if (err) {
+        return next(new AppError('Error signing token...', 500));
+      }
+
+      res.status(201).json({
+        status: 'success',
+        token,
+        data: {
+          user: newUser,
+        },
+      });
+    }
+  );
+*/
+
+/* to generate secret key for jwt token in console
+ node -e "console.log(require('crypto').randomBytes(64).toString('hex'));"*/
