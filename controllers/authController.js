ccaec15106bc08af920d36141a3ed9ab32301854
PRoy <sam469roger@gmail.com>
2023-05-27 14:46:14 +0530

    logging in users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+// sync version
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,10 +17,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  // sync version
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -24,28 +28,48 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 });
 
-/* async implementation
-  jwt.sign(
-    { id: newUser._id },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    },
-    (err, token) => {
-      if (err) {
-        return next(new AppError('Error signing token...', 500));
-      }
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
 
-      res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-          user: newUser,
-        },
-      });
-    }
-  );
-*/
+  // 1. check if email and password exists in the request
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  // 2. check if user exists and password is correct in db
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3. if everything is ok send token to client
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
 
 /* to generate secret key for jwt token in console
  node -e "console.log(require('crypto').randomBytes(64).toString('hex'));"*/
+
+/*
+// async version
+const signToken = (id) =>
+  new Promise((resolve, reject) => {
+    jwt.sign(
+      { id },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN },
+      (err, token) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(token);
+        }
+      }
+    );
+  });
+  */
