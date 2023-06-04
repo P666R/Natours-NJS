503a4d9d11a97d728b6dbb2f750609d4ca97bccc
PRoy <sam469roger@gmail.com>
2023-06-04 23:51:24 +0530

    set security http headers & limit body payload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,22 +11,31 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // global middlewares
+
+// set security http headers
+app.use(helmet());
+
+//  development logging
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// limit requests from same ip
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour',
 });
-
 app.use('/api', limiter);
 
-app.use(express.json());
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//  test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
