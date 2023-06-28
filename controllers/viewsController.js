2d4d9b52acc1feacd64babe66bf1f1d936bb3d7f
PRoy <sam469roger@gmail.com>
2023-06-28 17:22:59 +0530

    building tour page 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,8 +14,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1. get the data for the requested tour (incl reviews and tour guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  // 2. build template
+
+  // 3. render that template using the data from 1
   res.status(200).render('tour', {
     title: 'The forest hiker tour',
+    tour,
   });
-};
+});
