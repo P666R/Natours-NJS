f0f3f2749913c87ca8ead8fd49905aa354fe1a61
PRoy <sam469roger@gmail.com>
2023-07-02 21:27:06 +0530

    updating user password with api

--- public/js/index.js
+++ public/js/index.js
@@ -5,13 +5,14 @@ import 'regenerator-runtime/runtime';
 
 import { displayMap } from './leaflet.js';
 import { login, logout } from './login.js';
-import { updateData } from './updateSettings.js';
+import { updateSettings } from './updateSettings.js';
 
 // dom elements
 const leaflet = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // delegation
 if (leaflet) {
@@ -31,10 +32,29 @@ if (loginForm) {
 if (logoutBtn) logoutBtn.addEventListener('click', logout);
 
 if (userDataForm) {
-  addEventListener('submit', (e) => {
+  userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    updateData(name, email);
+    updateSettings({ name, email }, 'data');
+  });
+}
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,19 +2,22 @@
 
 import { showAlert } from './alerts.js';
 
-export const updateData = async (name, email) => {
+// type is either password or data
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://localhost:8000/api/v1/users/updateMyPassword'
+        : 'http://localhost:8000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:8000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data,
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully');
+      showAlert('success', `${type.toUpperCase()} updated successfully`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
